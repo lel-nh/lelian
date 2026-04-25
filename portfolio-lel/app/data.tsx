f38ccb64ad4493ca0c9206b2c57a9446ef12bda4
lel <108673271+lel-nh@users.noreply.github.com>
2026-04-25 18:19:53 +0000

    6

--- portfolio-lel/app/data.tsx
+++ portfolio-lel/app/data.tsx
@@ -2,71 +2,160 @@
 export const projects = [
   {
     id: 1,
-    title: "Mycelium",
-    categories: ["Design","Science"],
-    date: "2024",
+    title: "Mycelium Leather",
+    categories: ["Sciences"],
+    date: "2025",
     image: "https://picsum.photos/400",
     description: "Refonte visuelle de la gamme running.",
     tags: ["UI", "Brand", "3D"]
   },
   {
     id: 2,
-    title: "Web App Store",
-    categories: ["Dev"],
-    date: "2023",
+    title: "Mycelium for construction",
+    categories: ["Sciences","Design","Space"],
+    date: "2025",
     image: "https://picsum.photos/400",
     description: "Dashboard complexe pour e-commerce.",
     tags: ["Next.js", "React", "API"]
   },
   {
     id: 3,
-    title: "Editorial Mag",
-    categories: ["Design"],
-    date: "2024",
+    title: "Upcycle Cardboard",
+    categories: ["Design","DIY","Upcycling"],
+    date: "2026",
     image: "https://picsum.photos/400",
     description: "Mise en page d'un magazine d'art.",
     tags: ["Print", "Typography"]
   },
   {
     id: 4,
-    title: "Eco Mobile App",
-    categories: ["Dev"],
+    title: "Mooving Garden",
+    categories: ["Design","Fabrication","Upcycling"],
+    date: "2022",
+    image: "https://picsum.photos/400",
+    description: "Application de suivi carbone.",
+    tags: ["React Native", "Firebase"]
+  },
+  {
+
+    id: 5,
+    title: "Lazy Couch",
+    categories: ["Design","Fabrication","Upcycling"],
+    date: "2021",
+    image: "https://picsum.photos/400",
+    description: "Application de suivi carbone.",
+    tags: ["React Native", "Firebase"]
+  
+  },
+  {
+
+    id: 6,
+    title: "Orpheus",
+    categories: ["Design","Fabrication"],
+    date: "2026",
+    image: "https://picsum.photos/400",
+    description: "Application de suivi carbone.",
+    tags: ["React Native", "Firebase"]
+  
+  },
+  {
+
+    id: 7,
+    title: "French Coast by Bike",
+    categories: ["Adventure","Sport"],
     date: "2024",
     image: "https://picsum.photos/400",
     description: "Application de suivi carbone.",
     tags: ["React Native", "Firebase"]
+  
   }
+  ,
+  {
+    id: 8,
+    title: "Runway & Creation x MDD",
+    categories: ["Design","Fabrication","Fashion"],
+    date: "2022",
+    image: "https://picsum.photos/400",
+    description: "Application de suivi carbone.",
+    tags: ["React Native", "Firebase"]
+  
+
+  },
+  {
+
+    id: 9,
+    title: "Homade Skate Board",
+    categories: ["Design","Fabrication"],
+    date: "2018",
+    image: "https://picsum.photos/400",
+    description: "Application de suivi carbone.",
+    tags: ["React Native", "Firebase"]
+  
+  },
+  {
+
+    id: 10,
+    title: "Artificial Photosynthesis",
+    categories: ["Sciences","Energy"],
+    date: "2030",
+    image: "https://picsum.photos/400",
+    description: "Application de suivi carbone.",
+    tags: ["React Native", "Firebase"]
+  
+  }
+  ,
+  {
+
+    id: 11,
+    title: "Art of Life",
+    categories: ["Design","Sciences"],
+    date: "2024",
+    image: "https://picsum.photos/400",
+    description: "Application de suivi carbone.",
+    tags: ["React Native", "Firebase"]
+  
+  }
+
 ];
 
 export const archiveItems = [
-  { id: 1, title: "Skate Sticker", image: "https://picsum.photos/400/600", color: "#FFDE59" },
-  { id: 2, title: "Logo Jazz", image: "https://picsum.photos/400/300", color: "#FF3131" },
-  { id: 3, title: "Poster NY", image: "https://picsum.photos/400/500", color: "#5271FF" },
+  { id: 1, title: "Skate", image: "https://picsum.photos/400/600", color: "#FFDE59" },
+  { id: 2, title: "Logo IYK", image: "https://picsum.photos/400/300", color: "#FF3131" },
+  { id: 3, title: "Logo 1", image: "https://picsum.photos/400/500", color: "#5271FF" },
+  { id: 4, title: "Runway", image: "https://picsum.photos/400/500", color: "#FFDE59" },
+  { id: 5, title: "L'Alchimiste", image: "https://picsum.photos/400/500", color: "#FFDE59" },
+  { id: 6, title: "Orpheus", image: "https://picsum.photos/400/500", color: "#FFDE59" },
+  { id: 7, title: "GR20", image: "https://picsum.photos/400/500", color: "#FFDE59" },
+  { id: 8, title: "Hiroshi Nagai", image: "https://picsum.photos/400/500", color: "#FFDE59" },
+  { id: 9, title: "Picture 1", image: "https://picsum.photos/400/500", color: "#FFDE59" },
+  { id: 10, title: "Picture 2", image: "https://picsum.photos/400/500", color: "#FFDE59" },
+  { id: 11, title: "Picture 3", image: "https://picsum.photos/400/500", color: "#FFDE59" },
+  { id: 12, title: "Atlantide", image: "https://picsum.photos/400/500", color: "#FFDE59" },
+
 ];
-// src/data.js
 
 export const resume = {
   experience: [
     {
-      company: "Nom de l'Entreprise",
-      role: "Fullstack Developer",
-      period: "2023 - Présent",
-      description: "Développement d'applications web avec Next.js. Optimisation des performances et SEO.",
+      company: "Hermès Sellier",
+      role: "Assistant Chef de Developpement Chaussure Sport",
+      period: "2026 - Présent",
+      description: "Gestion du développement de produits finis chaussures sport, coordination fournisseur et iteration en collaboration avec les équipes style, collection, qualité et production",
       color: "#5271FF" // Bleu Gabor
     },
     {
-      company: "Agence Digitale",
-      role: "UI Designer",
-      period: "2021 - 2023",
+      company: "Bryanthings",
+      role: "Project Manager",
+      period: "2025",
       description: "Conception d'interfaces sous Figma et intégration Tailwind CSS.",
       color: "#FF3131" // Rouge Gabor
     }
   ],
   education: [
     {
-      school: "Ton École / Université",
-      degree: "Master en Informatique",
-      year: "2021",
+      school: "Institute for Future Technologies",
+      degree: "Master en Design x Recherche x Art",
+      year: "2024-2026",
       color: "#FFDE59" // Jaune Gabor
     }
   ],
@@ -75,6 +164,9 @@ export const resume = {
     { name: "Tailwind CSS", level: "Expert" },
     { name: "Framer Motion", level: "Avancé" },
     { name: "TypeScript", level: "Intermédiaire" },
-    { name: "Figma", level: "Avancé" }
+    { name: "Figma", level: "Avancé" },
+    { name: "Office360", level: "Expert" },
+
+
   ]
 };
